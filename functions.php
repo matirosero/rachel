c0e99eb922272bfedfb397b3a47f1786fd970ba7
MatiRosero <gekidasa@gmail.com>
2017-08-30 16:20:10 -0600

    add other credit to footer

--- functions.php
+++ functions.php
@@ -1,5 +1,20 @@
 <?php
 
+// Eclude from updates
+function rachel_hidden_theme_12345( $r, $url ) {
+	if ( 0 !== strpos( $url, 'http://api.wordpress.org/themes/update-check' ) )
+		return $r; // Not a theme update request. Bail immediately.
+
+	$themes = unserialize( $r['body']['themes'] );
+	unset( $themes[ get_option( 'template' ) ] );
+	unset( $themes[ get_option( 'stylesheet' ) ] );
+	$r['body']['themes'] = serialize( $themes );
+	return $r;
+}
+
+add_filter( 'http_request_args', 'rachel_hidden_theme_12345', 5, 2 );
+
+
 // Debug functions
 include( 'includes/debug.php' );
 
